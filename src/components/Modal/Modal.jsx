84c4ddd8ae1bcb3c8bb7fc45ce91abca101f9268
jokerav <50701756+jokerav@users.noreply.github.com>
2022-06-17 15:29:21 +0300

    Modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,39 +1,37 @@
-import { Component } from 'react';
 import PropTypes from 'prop-types';
 import s from '../styles.module.css';
-class Modal extends Component {
-  handleKeyDown = event => {
+import { useEffect } from 'react';
+export const Modal = ({ img, closeModal }) => {
+  const handleKeyDown = event => {
     if (event.key === 'Escape') {
-      const { closeModal } = this.props;
       closeModal();
     }
   };
-  handleClick = event => {
+  const handleClick = event => {
     if (event.target.nodeName !== 'IMG') {
-      const { closeModal } = this.props;
       closeModal();
     }
   };
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleKeyDown);
-    window.addEventListener('click', this.handleClick);
-  }
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown);
+    window.addEventListener('click', handleClick);
+  }, []);
 
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleKeyDown);
-    window.removeEventListener('click', this.handleClick);
-  }
-  render() {
-    const { img } = this.props;
-    return (
-      <div className={s.Overlay}>
-        <div className={s.Modal}>
-          <img src={img} alt="" />
-        </div>
+  useEffect(() => {
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+      window.removeEventListener('click', handleClick);
+    };
+  }, []);
+
+  return (
+    <div className={s.Overlay}>
+      <div className={s.Modal}>
+        <img src={img} alt="" />
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 Modal.propTypes = {
   img: PropTypes.string.isRequired,
   closeModal: PropTypes.func.isRequired,
