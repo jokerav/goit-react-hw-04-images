0a94e4fb01b56d49483cbc7fb10dda30bf7a8e3a
jokerav <50701756+jokerav@users.noreply.github.com>
2022-06-18 12:52:50 +0300

    Update ImageGallery.jsx

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,30 +1,27 @@
-import { Component } from 'react';
+// import { Component } from 'react';
 import s from '../styles.module.css';
 import PropTypes from 'prop-types';
 import ImageGalleryItem from 'components/ImageGalleryItem/ImageGalleryItem';
 
-class ImageGallery extends Component {
-  render() {
-    const { onImageClick, images } = this.props;
-    return (
-      <div onClick={e => onImageClick(e)}>
-        <ul className={s.ImageGallery}>
-          {images.map(img => {
-            const { id, webformatURL, largeImageURL } = img;
-            return (
-              <ImageGalleryItem
-                key={id}
-                id={id}
-                webformatURL={webformatURL}
-                largeImageURL={largeImageURL}
-              />
-            );
-          })}
-        </ul>
-      </div>
-    );
-  }
-}
+const ImageGallery = ({ onImageClick, images }) => {
+  return (
+    <div onClick={e => onImageClick(e)}>
+      <ul className={s.ImageGallery}>
+        {images.map(img => {
+          const { id, webformatURL, largeImageURL } = img;
+          return (
+            <ImageGalleryItem
+              key={id}
+              id={id}
+              webformatURL={webformatURL}
+              largeImageURL={largeImageURL}
+            />
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
 ImageGallery.propTypes = {
   images: PropTypes.arrayOf(
     PropTypes.exact({
